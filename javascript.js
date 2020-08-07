87934d5e373f4b1a08e4737312681199bfaaafd4
James McCarty <jamesmccarty126@gmail.com>
2020-08-06 23:31:48 -0700

    js update

--- javascript.js
+++ javascript.js
@@ -34,122 +34,122 @@ const shitakeDiv = document.querySelector(".checkout-shitake");
 let mainContent = document.querySelector(".wrap");
 let mobilePopUp = document.querySelector(".mobile-pop-up");
 
-checkCart(0);
-checkCart(1);
-checkCheckout();
-update();
+// checkCart(0);
+// checkCart(1);
+// checkCheckout();
+// update();
 
 //UPDATES AND CHECKING localStorage
 
     function update() {
-
-      // getQuantity += getEnokiInCart;
-
-      if(enokiQuantity !== null) {
-      enokiQuantity.innerHTML = getQuantity;
-    }
-
-    if(shitakeQuantity !== null) {
-    shitakeQuantity.innerHTML = getQuantity;
-    }
+    //
+    //   // getQuantity += getEnokiInCart;
+    //
+    //   if(enokiQuantity !== null) {
+    //   enokiQuantity.innerHTML = getQuantity;
+    // }
+    //
+    // if(shitakeQuantity !== null) {
+    // shitakeQuantity.innerHTML = getQuantity;
+    // }
     }
 
     function checkCheckout() {
-          if(enokiDiv !== null) {
-          if(mushrooms.enoki === "true") {
-            enokiDiv.style.display = "block";
-          } else {
-            enokiDiv.style.display = "none";
-          }
-        }
-
-        if(getQuantity === 0) {
-          mushrooms.enoki = "false";
-          mushrooms.enoki = localStorage.getItem("enoki");
-        }
+        //   if(enokiDiv !== null) {
+        //   if(mushrooms.enoki === "true") {
+        //     enokiDiv.style.display = "block";
+        //   } else {
+        //     enokiDiv.style.display = "none";
+        //   }
+        // }
+        //
+        // if(getQuantity === 0) {
+        //   mushrooms.enoki = "false";
+        //   mushrooms.enoki = localStorage.getItem("enoki");
+        // }
 
         }
 
     function checkCart() {
-
-    localStorage.getItem("itemStats-itemQuantity");
-    localStorage.getItem("itemStats-enokiInCart");
-    getIcon();
-    return [localStorage.setItem("itemStats-itemQuantity", getQuantity), localStorage.setItem("itemStats-enokiInCart", getEnokiInCart)];
+    //
+    // localStorage.getItem("itemStats-itemQuantity");
+    // localStorage.getItem("itemStats-enokiInCart");
+    // getIcon();
+    // return [localStorage.setItem("itemStats-itemQuantity", getQuantity), localStorage.setItem("itemStats-enokiInCart", getEnokiInCart)];
     }
 
 
 
 function getIcon() {
-  if(getQuantity === 0) {
-      cartPin.style.display = "none";
-      mushrooms.enoki = localStorage.getItem("enoki");
-        mushrooms.enoki = "false";
-        if(enokiDiv !== null) {
-         enokiDiv.style.display = "none";
-         totalCostDollars = 0;
-         totalCostCents = 0;
-         totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}0`;
-       }
-       if (shitakeDiv !== null) {
-         shitakeDiv.style.display = "none";
-         totalCostDollars = 0;
-         totalCostCents = 0;
-         totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}0`;
-       }
-
-  } else if(getQuantity === 1) {
-    cartPin.src="icons/one.png";
-      cartPin.style.display = "block";
-        if(totalCostElement !== null) {
-      totalCostDollars = enokiCostDollars;
-      totalCostCents = enokiCostCents;
-        ifNumbBigger();
-      totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
-    }
-    } else if (getQuantity === 2) {
-      cartPin.src="icons/two.png";
-      cartPin.style.display = "block";
-      if(totalCostElement !== null) {
-      totalCostDollars = enokiCostDollars * getQuantity;
-      totalCostCents = enokiCostCents * getQuantity;
-        ifNumbBigger();
-      totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
-    }
-    }  else if (getQuantity === 3) {
-      cartPin.src="icons/three.png";
-      cartPin.style.display = "block";
-      if(totalCostElement !== null) {
-      totalCostDollars = enokiCostDollars * getQuantity;
-      totalCostCents = enokiCostCents * getQuantity;
-        ifNumbBigger();
-      console.log("CENTS" + totalCostCents);
-      totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
-    }
-  }  else if(getQuantity < 0) {
-      getQuantity = 0;
-  } else {
-    alert("cart is at max capacity");
-    getQuantity = 3;
-  }
+  // if(getQuantity === 0) {
+  //     cartPin.style.display = "none";
+  //     mushrooms.enoki = localStorage.getItem("enoki");
+  //       mushrooms.enoki = "false";
+  //       if(enokiDiv !== null) {
+  //        enokiDiv.style.display = "none";
+  //        totalCostDollars = 0;
+  //        totalCostCents = 0;
+  //        totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}0`;
+  //      }
+  //      if (shitakeDiv !== null) {
+  //        shitakeDiv.style.display = "none";
+  //        totalCostDollars = 0;
+  //        totalCostCents = 0;
+  //        totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}0`;
+  //      }
+  //
+  // } else if(getQuantity === 1) {
+  //   cartPin.src="icons/one.png";
+  //     cartPin.style.display = "block";
+  //       if(totalCostElement !== null) {
+  //     totalCostDollars = enokiCostDollars;
+  //     totalCostCents = enokiCostCents;
+  //       ifNumbBigger();
+  //     totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
+  //   }
+  //   } else if (getQuantity === 2) {
+  //     cartPin.src="icons/two.png";
+  //     cartPin.style.display = "block";
+  //     if(totalCostElement !== null) {
+  //     totalCostDollars = enokiCostDollars * getQuantity;
+  //     totalCostCents = enokiCostCents * getQuantity;
+  //       ifNumbBigger();
+  //     totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
+  //   }
+  //   }  else if (getQuantity === 3) {
+  //     cartPin.src="icons/three.png";
+  //     cartPin.style.display = "block";
+  //     if(totalCostElement !== null) {
+  //     totalCostDollars = enokiCostDollars * getQuantity;
+  //     totalCostCents = enokiCostCents * getQuantity;
+  //       ifNumbBigger();
+  //     console.log("CENTS" + totalCostCents);
+  //     totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
+  //   }
+  // }  else if(getQuantity < 0) {
+  //     getQuantity = 0;
+  // } else {
+  //   alert("cart is at max capacity");
+  //   getQuantity = 3;
+  // }
 }
 
 function ifNumbBigger() {
-  if (totalCostCents >= 200) {
-    totalCostCents -= 200;
-    totalCostDollars += 2;
-  }
-  if(totalCostCents >= 100) {
-      totalCostCents -= 100;
-      totalCostDollars += 1;
-  }
+  // if (totalCostCents >= 200) {
+  //   totalCostCents -= 200;
+  //   totalCostDollars += 2;
+  // }
+  // if(totalCostCents >= 100) {
+  //     totalCostCents -= 100;
+  //     totalCostDollars += 1;
+  // }
 }
 
 function mathProbs() {
-  if(enokiCostCents >= 100) {
-    enokiCostDollars += 1;
-    enokiCostCents = 0;
-  }
+  // if(enokiCostCents >= 100) {
+  //   enokiCostDollars += 1;
+  //   enokiCostCents = 0;
+  // }
 }
 
 function underConstruction() {
@@ -160,15 +160,15 @@ function underConstruction() {
 //ADDING ITEMS TO CART
 
 function addToCart() {
-  if(getQuantity < 3) {
-          console.log("lshjhdjgh");
-  getQuantity += 1;
-  itemStats.itemQuantity = getQuantity;
-  // console.log(itemStats.itemQuantity);
-  getIcon();
-  return localStorage.setItem("itemStats-itemQuantity", getQuantity);
+  // if(getQuantity < 3) {
+  //         console.log("lshjhdjgh");
+  // getQuantity += 1;
+  // itemStats.itemQuantity = getQuantity;
+  // // console.log(itemStats.itemQuantity);
+  // getIcon();
+  // return localStorage.setItem("itemStats-itemQuantity", getQuantity);
 }
-}
+
 
 // function addEnoki() {
 //     getEnokiInCart += 1;
@@ -182,15 +182,15 @@ function addToCart() {
 
 
 function removeItems() {
-if (getQuantity >= 0) {
-  if(getQuantity > 0) {
-  getQuantity = 0;
-  enokiQuantity.innerHTML = `${getQuantity}`;
-}
-console.log(getQuantity);
-getIcon();
-}
-return [localStorage.setItem("itemStats-itemQuantity", getQuantity), localStorage.setItem("enoki", mushrooms.enoki)];
+// if (getQuantity >= 0) {
+//   if(getQuantity > 0) {
+//   getQuantity = 0;
+//   enokiQuantity.innerHTML = `${getQuantity}`;
+// }
+// console.log(getQuantity);
+// getIcon();
+// }
+// return [localStorage.setItem("itemStats-itemQuantity", getQuantity), localStorage.setItem("enoki", mushrooms.enoki)];
 }
 
 
@@ -198,54 +198,54 @@ return [localStorage.setItem("itemStats-itemQuantity", getQuantity), localStorag
 
 
 function addEnokiNum() {
-  if(getQuantity < 3) {
-    getQuantity += 1;
-    enokiQuantity.innerHTML = `${getQuantity}`;
-    getIcon();
-    totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
-    itemStats.itemQuantity = getQuantity;
-    console.log(itemStats.itemQuantity);
-    return localStorage.setItem("itemStats-itemQuantity", getQuantity);
-  }
+  // if(getQuantity < 3) {
+  //   getQuantity += 1;
+  //   enokiQuantity.innerHTML = `${getQuantity}`;
+  //   getIcon();
+  //   totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
+  //   itemStats.itemQuantity = getQuantity;
+  //   console.log(itemStats.itemQuantity);
+  //   return localStorage.setItem("itemStats-itemQuantity", getQuantity);
+  // }
   }
 
   function addShitakeNum() {
-    if(getQuantity < 3) {
-      if(getQuantity < 0) {
-        getQuantity = 0;
-      }
-      getQuantity += 1;
-      shitakeQuantity.innerHTML = `${getQuantity}`;
-      getIcon();
-      totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
-      itemStats.itemQuantity = getQuantity;
-      console.log(itemStats.itemQuantity);
-      return localStorage.setItem("itemStats-itemQuantity", getQuantity);
-    }
+    // if(getQuantity < 3) {
+    //   if(getQuantity < 0) {
+    //     getQuantity = 0;
+    //   }
+    //   getQuantity += 1;
+    //   shitakeQuantity.innerHTML = `${getQuantity}`;
+    //   getIcon();
+    //   totalCostElement.innerHTML = `TOTAL: $${totalCostDollars}.${totalCostCents}`;
+    //   itemStats.itemQuantity = getQuantity;
+    //   console.log(itemStats.itemQuantity);
+    //   return localStorage.setItem("itemStats-itemQuantity", getQuantity);
+    // }
     }
 
 //REMOVE ITEMS FROM CART
 
 function removeEnokiNum() {
-if(getQuantity < 4) {
-  getQuantity -= 1;
-  enokiQuantity.innerHTML = `${getQuantity}`;
-  getIcon();
-  itemStats.itemQuantity = getQuantity;
-  console.log(itemStats.itemQuantity);
-  return localStorage.setItem("itemStats-itemQuantity", getQuantity);
-}
+// if(getQuantity < 4) {
+//   getQuantity -= 1;
+//   enokiQuantity.innerHTML = `${getQuantity}`;
+//   getIcon();
+//   itemStats.itemQuantity = getQuantity;
+//   console.log(itemStats.itemQuantity);
+//   return localStorage.setItem("itemStats-itemQuantity", getQuantity);
+// }
 }
 
     function removeShitakeNum() {
-    if(getQuantity < 4) {
-      getQuantity -= 1;
-      shitakeQuantity.innerHTML = `${getQuantity}`;
-      getIcon();
-      itemStats.itemQuantity = getQuantity;
-      console.log(itemStats.itemQuantity);
-      return localStorage.setItem("itemStats-itemQuantity", getQuantity);
-    }
+    // if(getQuantity < 4) {
+    //   getQuantity -= 1;
+    //   shitakeQuantity.innerHTML = `${getQuantity}`;
+    //   getIcon();
+    //   itemStats.itemQuantity = getQuantity;
+    //   console.log(itemStats.itemQuantity);
+    //   return localStorage.setItem("itemStats-itemQuantity", getQuantity);
+    // }
     }
 
     //ECT
